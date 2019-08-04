cb79f9b246fb4d43fe4b911228aa29d18f42e574
irevoire <irevoire@hotmail.fr>
2019-08-04 21:18:09 +0200

    move all the motherboard directory to the root

--- gameboy-core/src/lib.rs
+++ gameboy-core/src/lib.rs
@@ -1,1 +1,12 @@
-pub mod motherboard;
+mod cartridge;
+mod cpu;
+mod memory;
+mod registers;
+
+use self::cpu::Cpu;
+use self::memory::Memory;
+
+pub struct Motherboard {
+    cpu: Cpu,
+    memory: Memory,
+}
